0839b28ac862514e27c1a2f8c23222267a321a78
nikuze <nikuzediop@gmail.com>
2022-02-16 12:44:32 +0200

    complex layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        display: "flex",
+        marginTop: "70px",
+        width: "100%",
+      }}
+    >
+      <div style={{ height: "550px", width: "60%", position: "relative" }}>
+        <img
+          src="./images/imagesix.jpg"
+          alt=""
+          style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            bottom: "0",
+            color: "white",
+            fontSize: "22px",
+            padding: "20px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Un partenariat de grand prestige avec un contrat colossal pour la
+          ligue nationale de Basketball
+        </div>
+      </div>
+
+      <div style={{ width: "40%" }}>
+        <div
+          style={{
+            height: "275px",
+            position: "relative",
+          }}
+        >
+          <img
+            src="./images/imagecinq.jpg"
+            alt=""
+            style={{ objectFit: "cover", width: "100%", height: "100%" }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              bottom: "0",
+              color: "white",
+              fontSize: "22px",
+              padding: "20px",
+            }}
+          >
+            Incroyable, les Gazelles remporte la coupe VIVA Champions
+          </div>
+        </div>
+        <div style={{ height: "275px", width: "100%", position: "relative" }}>
+          <img
+            src="./images/imagequatres.jpg"
+            alt=""
+            style={{ objectFit: "cover", width: "100%", height: "100%" }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              bottom: "0",
+              color: "white",
+              fontSize: "22px",
+              padding: "20px",
+            }}
+          >
+            FEBABU en collaboration avec Brarudi organisent Shoota na Viva
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
