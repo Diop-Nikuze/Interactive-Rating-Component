6e3ec077d0534b438ae6ea79eee6e927a31b07eb
nikuze <nikuzediop@gmail.com>
2022-02-24 19:53:14 +0200

    recap on trendpost component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useColorModeValue } from "@chakra-ui/react";
 
-import {
-  // Box,
-  // Container,
-  // HStack,
-  // Text,
-  // VStack,
-  // Image,
-  // useColorModeValue,
-  ChakraProvider,
-} from "@chakra-ui/react";
-
-// import vivamalt from "./image/vivamalt.jpg";
+import vivamalt from "./image/vivamalt.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 function App() {
   const [data, setData] = useState([]);
   // const colorLightDark = useColorModeValue("#006159", "#2ab391");
   // const colorContent = useColorModeValue("#686868", "#aeb2c0");
   // const readMoreColor = useColorModeValue("black", "#aeb2c0");
-  // const colorBorder = useColorModeValue("#ddd", "#2D3748");
-  // const formatDate = new Intl.DateTimeFormat("fr", {
-  //   year: "numeric",
-  //   month: "short",
-  //   day: "numeric",
-  // });
+  const colorBorder = useColorModeValue("#ddd", "#2D3748");
+  const formatDate = new Intl.DateTimeFormat("fr", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
 
   useEffect(() => {
     axios.get("https://fortheloveofthegame.herokuapp.com/posts").then((res) => {
@@ -39,37 +31,42 @@ function App() {
   );
 
   return (
-    <ChakraProvider>
-      <ul className="list-news">
-        {sortedPosts.slice(3, 6).map((item, id) => (
-          <li key={id}>
-            <a
-              href="exemple.com"
-              style={{ display: "flex", alignItems: "center" }}
+    <Container style={{ margin: "20px auto" }}>
+      <div className="box-posts">
+        <div>
+          {sortedPosts.slice(3, 6).map((item, id) => (
+            <div
+              key={id}
+              className="box-container"
+              style={{
+                borderBottom: `${
+                  id === 0 || id === 1 ? `1px solid ${colorBorder}` : "none"
+                }`,
+              }}
             >
-              <div
-                style={{ width: "250px", height: "250px", marginRight: "20px" }}
-                className="box-image"
-              >
-                <img src={item.image.formats.medium.url} alt="" />
+              <div className="container-image">
+                <img
+                  src={item.image.formats.medium.url}
+                  alt=""
+                  className="image"
+                />
               </div>
-              <div>
-                <h2
-                  style={{
-                    padding: "4px 6px",
-                    background: "#006159",
-                    display: "inline-block",
-                  }}
-                >
-                  {item.category}
-                </h2>
-                <h2>{item.title}</h2>
+              <div className="content">
+                <p>{item.category}</p>
+                <p className="title">{item.title}</p>
+                <p className="content-descr">
+                  {item.content.substring(0, 110)}...
+                </p>
+                <p> {formatDate.format(new Date(item.date))}</p>
               </div>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </ChakraProvider>
+            </div>
+          ))}
+        </div>
+        <div className="ads">
+          <img src={vivamalt} alt="" className="ads-img" />
+        </div>
+      </div>
+    </Container>
   );
 }
 
